633ae1487981d7cf3f7e06b80973014659930831
Mategrze <mgrzelak1997@wp.pl>
2019-03-27 20:31:07 +0100

    Moje rozwiazanie zadania 5

---
+++ pack/Main.java
@@ -0,0 +1,50 @@
+package pack;
+
+import java.util.stream.IntStream;
+
+
+public class Main implements FizzBuzz{
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.print(0, 15);
+	}
+	
+	/*@Override
+	public void print(int from, int to) {
+		for (int i = from; i <= to; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("" + i + " - " + "FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("" + i + " - " + "Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("" + i + " - " + "Buzz");
+			}
+		}
+	}*/
+
+	/*@Override
+	public void print(int from, int to) {
+		IntStream.range(from, to).forEach(i -> {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("" + i + " - " + "FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("" + i + " - " + "Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("" + i + " - " + "Buzz");
+			}
+		});
+	}*/
+	
+	@Override
+	public void print(int from, int to) {
+		for (int i : IntStream.range(from, to).toArray())
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("" + i + " - " + "FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("" + i + " - " + "Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("" + i + " - " + "Buzz");
+			}
+	};
+}
