b1d25b71d03ffa83020cb722e5759d974ebbe818
olektomek <olekstomek@gmail.com>
2019-03-27 22:38:41 +0100

    propozycja rozwiazania z implementacja 3

--- src/ThirdImplementation.java
+++ src/ThirdImplementation.java
@@ -3,8 +3,27 @@ public class ThirdImplementation implements FizzBuzz {
         ThirdImplementation thirdImplementation = new ThirdImplementation();
         thirdImplementation.print(0, 15);
     }
+
     @Override
     public void print(int from, int to) {
-       //TODO
+        int[] array = createArrayWithNumbers(from, to);
+
+        for (int number : array) {
+            if (number % 3 == 0 && number % 5 == 0)
+                System.out.println(number + " - FizzBuzz");
+            else if (number % 5 == 0)
+                System.out.println(number + " - Buzz");
+            else if (number % 3 == 0)
+                System.out.println(number + " - Fizz");
+        }
+    }
+
+    private int[] createArrayWithNumbers(int from, int to) {
+        int[] array = new int[to + 1];
+        for (int i = 0; i <= to; i++) {
+            array[i] = from;
+            from++;
+        }
+        return array;
     }
 }
