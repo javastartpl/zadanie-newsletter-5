42368b984e3b8e5a481d3c892aac4183d9a2cbd7
olektomek <olekstomek@gmail.com>
2019-04-03 21:51:02 +0200

    poprawki zgodnie z uwagami na wideo YT

--- src/FirstImplementation.java
+++ src/FirstImplementation.java
@@ -1,18 +1,9 @@
 public class FirstImplementation implements FizzBuzz {
-    public static void main(String[] args) {
-        FirstImplementation firstImplementation = new FirstImplementation();
-        firstImplementation.print(0, 15);
-    }
 
     @Override
     public void print(int from, int to) {
         while (from <= to) {
-            if (from % 3 == 0 && from % 5 == 0)
-                System.out.println(from + " - FizzBuzz");
-            else if (from % 5 == 0)
-                System.out.println(from + " - Buzz");
-            else if (from % 3 == 0)
-                System.out.println(from + " - Fizz");
+            FizzBuzzChecker.checkDivisibility(from);
             from += 1;
         }
     }
--- src/ThirdImplementation.java
+++ src/ThirdImplementation.java
@@ -1,21 +1,11 @@
 public class ThirdImplementation implements FizzBuzz {
-    public static void main(String[] args) {
-        ThirdImplementation thirdImplementation = new ThirdImplementation();
-        thirdImplementation.print(0, 15);
-    }
 
     @Override
     public void print(int from, int to) {
         int[] array = createArrayWithNumbers(from, to);
 
-        for (int number : array) {
-            if (number % 3 == 0 && number % 5 == 0)
-                System.out.println(number + " - FizzBuzz");
-            else if (number % 5 == 0)
-                System.out.println(number + " - Buzz");
-            else if (number % 3 == 0)
-                System.out.println(number + " - Fizz");
-        }
+        for (int number : array)
+            FizzBuzzChecker.checkDivisibility(number);
     }
 
     private int[] createArrayWithNumbers(int from, int to) {
